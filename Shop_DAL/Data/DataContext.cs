ea901b342d6a71e947cca36af1734810a950b07b
ilushegg <marnopolskiyi@mail.ru>
2022-09-01 21:16:17 +0300

    Initialized the database

---
+++ Shop_DAL/Data/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop_Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_DAL.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Product> Products { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ Shop_Data/Enum/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop_Domain.Enum
+{
+    public enum Role
+    {
+        ADMIN,
+        MODERATOR,
+        USER
+    }
+}
